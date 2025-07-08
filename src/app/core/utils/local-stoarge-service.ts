f32d4d62f91f5ae6c4422adaa51ed259d5b99787
Schadrac SODE <schadrac.sode@gmail.com>
2025-07-08 21:31:49 +0100

    test

--- src/app/core/utils/local-stoarge-service.ts
+++ src/app/core/utils/local-stoarge-service.ts
@@ -2,41 +2,35 @@ import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root',
+})
 export class LocalStorageService {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-    
-    set(key: string, value: string) {
-        if (isPlatformBrowser(this.platformId)) {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
+  set(key: string, value: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(key, JSON.stringify(value));
     }
+  }
 
-    get(key: string) {
-        let el:any=""
-        if (isPlatformBrowser(this.platformId)) {
-             el=localStorage.getItem(key)
-             return el !=null ? JSON.parse(el):null;
-
-         }
-
-         return null
+  get(key: string) {
+    let el: any = '';
+    if (isPlatformBrowser(this.platformId)) {
+      el = localStorage.getItem(key);
+      return el != null ? JSON.parse(el) : null;
     }
+    return null;
+  }
 
-    remove(key: string) {
-        if (isPlatformBrowser(this.platformId)) {
-
-        localStorage.removeItem(key);
-        }
+  remove(key: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(key);
     }
+  }
 
-    clear() {
-        if (isPlatformBrowser(this.platformId)) {
-
-        localStorage.clear();
-        }
+  clear() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear();
     }
+  }
 }
