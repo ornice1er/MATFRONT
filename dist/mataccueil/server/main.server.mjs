2fd6385fdd28dafbd22348cc4a22e430d6f04135
Schadrac SODE <schadrac.sode@gmail.com>
2025-07-28 13:30:47 +0100

    fix(MATACCUEIL): fix build

--- dist/mataccueil/server/main.server.mjs
+++ dist/mataccueil/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VKEI6XKL.mjs";import"./chunk-JTKLYYPQ.mjs";import"./chunk-NTJ66CMW.mjs";import"./chunk-UXFCX5JO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KWOTZ45R.mjs";import"./chunk-JTKLYYPQ.mjs";import"./chunk-NTJ66CMW.mjs";import"./chunk-UXFCX5JO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
