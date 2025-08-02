aa1be0d3f542f5184d94b37e1152ae04401c6eaa
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 <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>
2025-08-02 11:01:24 +0100

    update

---
+++ dist/mataccueil/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-S3BKFP74.mjs";import"./chunk-5ZNB3C6S.mjs";import"./chunk-BCZYVDX7.mjs";import"./chunk-ULS2FFY5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
