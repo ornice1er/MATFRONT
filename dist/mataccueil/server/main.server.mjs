f3af61f44fb83aacebf24a7e008408448eab0fc9
Schadrac SODE <schadrac.sode@gmail.com>
2025-08-04 23:25:58 +0100

    fix(MATACCUEIL):deploiment

--- dist/mataccueil/server/main.server.mjs
+++ dist/mataccueil/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HWP3VFHW.mjs";import"./chunk-5ZNB3C6S.mjs";import"./chunk-BCZYVDX7.mjs";import"./chunk-ULS2FFY5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-23ZIWL2B.mjs";import"./chunk-ELZW4CJB.mjs";import"./chunk-SVM4M5OE.mjs";import"./chunk-UXFCX5JO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
