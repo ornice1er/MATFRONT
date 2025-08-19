8552ac41872007e368d4347695e0a6edb163e47c
Schadrac SODE <schadrac.sode@gmail.com>
2025-08-19 19:54:35 +0100

    fix(MATACCUEIL): add active directory

--- dist/mataccueil/server/main.server.mjs
+++ dist/mataccueil/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XF4TOXGH.mjs";import"./chunk-C3IPGRAP.mjs";import"./chunk-DWO2GW7G.mjs";import"./chunk-UXFCX5JO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BZYOM4BI.mjs";import"./chunk-C3IPGRAP.mjs";import"./chunk-DWO2GW7G.mjs";import"./chunk-UXFCX5JO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
