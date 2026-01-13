b270e1f184e8f573e383629395db9e1ba20071c4
Ornice <ornihouss1@gmail.com>
2026-01-13 11:52:14 +0100

    fix data

--- dist/mataccueil/server/main.server.mjs
+++ dist/mataccueil/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IYEJJXDQ.mjs";import"./chunk-WAYEDS54.mjs";import"./chunk-PEAYCXWP.mjs";import"./chunk-ULS2FFY5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2ZN6DDHE.mjs";import"./chunk-WAYEDS54.mjs";import"./chunk-PEAYCXWP.mjs";import"./chunk-ULS2FFY5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
